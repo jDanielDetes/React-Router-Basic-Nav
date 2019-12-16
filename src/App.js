82593321432ed75df9583f77093afd91238a21b8
jDanielDetes <jdanieldetes@gmail.com>
2019-12-16 16:16:36 -0500

    assignment finished

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 import {Route} from "react-router-dom"
 
+
 const App = () => (
   <div>
     <Navigation />
-    
+    <Route exact path = "/">
+      <Home />
+    </Route>
+    <Route exact path = "/About">
+      <About/>
+    </Route>
+    <Route exact path ="/Contact">
+      <Contact/>
+    </Route>
   </div>
 );
 
